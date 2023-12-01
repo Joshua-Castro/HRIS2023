f46506767f549a4064f0e457e6e17055e70161d0
Joshua D. Castro <jdcastro@prompt.ppa>
2023-12-01 16:44:39 +0800

    PARTIAL COMMIT: START THE UI SETUP OR DESIGN FOR THE SETUP PAYROLL

--- resources/views/modals/payroll-generate-modal.blade.php
+++ resources/views/modals/payroll-generate-modal.blade.php
@@ -1,48 +1,38 @@
 <div class="modal fade" id="generate-payroll-modal" tabindex="-1" role="dialog" aria-labelledby="generatePayrollModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-dialog" role="document" style="max-width: 3050px; width: 1000px;">
       <div class="modal-content">
         <div class="modal-body">
             <div class="row mt-2">
                 <span class="fw-bold text-center" style="font-size: 26px;">PAYROLL FOR 11/01/2023 - 11/15/2023</span>
             </div>
-            <div class="row mt-2">
+            <div class="row mt-4">
                 <div class="table-sm table-responsive mt-1">
                     <table class="table table-striped table-bordered">
-                    <thead>
-                        <tr>
-                            <th>Date</th>
-                            <th class="text-center">CLOCK IN</th>
-                            <th class="text-center">CLOCK OUT</th>
-                            <th class="text-center">NOTES</th>
-                            <th class="text-center">WH</th>
-                            <th class="text-center">RWH</th>
-                            <th class="text-center">OTH</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr class="p-0">
-                            <td x-text="rows.attendance_date"></td>
-                            <td class="text-center py-0">SAMPLE DATA</td>
-                            <td class="text-center py-0">SAMPLE DATA</td>
-                            <td class="text-center py-0 text-wrap" x-text="(rows.notes ? rows.notes : '-')"></td>
-                            <td class="text-center py-0">SAMPLE DATA</td>
-                            <td class="text-center py-0">SAMPLE DATA</td>
-                            <td class="text-center py-0">SAMPLE DATA</td>
-                        </tr>
-                    </tbody>
-                    <thead x-show="(attendancePayrollData ?? []).length > 0">
-                        <tr>
-                            <th colspan="7" class="text-center fw-bold" style="letter-spacing: 5px; font-size: 16px;">TOTAL</th>
-                        </tr>
-                    </thead>
-                    <tbody x-show="(attendancePayrollData ?? []).length > 0">
-                        <tr>
-                            <td colspan="4"></td>
-                            <td class="text-center">SAMPLE DATA CHANGE</td>
-                            <td class="text-center">SAMPLE DATA</td>
-                            <td class="text-center">SAMPLE DATA</td>
-                        </tr>
-                    </tbody>
+                        <thead>
+                            <tr>
+                                <th colspan="4" class="text-center py-2 fw-bold" style="letter-spacing: 5px; font-size: 16px;">EMPLOYEE INFORMATION</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="py-2 fw-bold" style="max-width: 500px; width: 90px;">Name :</td>
+                                <td class="py-2">Joshua Dugaduga Castro</td>
+                                <td class="py-2 fw-bold" style="max-width: 500px; width: 90px;">Employee No :</td>
+                                <td class="py-2">012345678999</td>
+                            </tr>
+                            <tr>
+                                <td class="py-2 fw-bold" style="max-width: 500px; width: 90px;">Basic salary :</td>
+                                <td class="py-2">30,458.10</td>
+                                <td class="py-2 fw-bold" style="max-width: 500px; width: 90px;">Department :</td>
+                                <td class="py-2">Information Technology</td>
+                            </tr>
+                            <tr>
+                                <td class="py-2 fw-bold" style="max-width: 500px; width: 90px;">Date Hired :</td>
+                                <td class="py-2">2023-09-18</td>
+                                <td class="py-2 fw-bold" style="max-width: 500px; width: 90px;">Status :</td>
+                                <td class="py-2">Regular</td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
